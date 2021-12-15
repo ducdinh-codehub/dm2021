d64ce073bb67b5977e702157fbd60c6c8f8c8362
ducdinh-codehub <dinhduc31197@gmail.com>
2021-12-15 18:22:11 +0700

    Meanshift with dynamic bandwidth

--- 05_meanshift.py
+++ 05_meanshift.py
@@ -21,10 +21,18 @@ X = np.array([
 colors = 10 * ["g", "r", "c", "b", "k"]
 
 class Mean_Shift:
-    def __init__(self, bandwidth=4):
+    def __init__(self, bandwidth=None, bandwidth_norm_step=100):
         self.bandwidth = bandwidth
+        self.bandwidth_norm_step = bandwidth_norm_step
 
     def fit(self, data):
+
+        if self.bandwidth == None:
+            all_data_centroid = np.average(data, axis=0)
+            all_data_norm = np.linalg.norm(all_data_centroid)
+            self.bandwidth = all_data_norm / self.bandwidth_norm_step
+
+
         centroids = {}
 
         for i in range(len(data)):
@@ -35,13 +43,42 @@ class Mean_Shift:
             for i in centroids:
                 in_bandwidth = []
                 centroid  = centroids[i]
+
+                weights = [i for i in range(self.bandwidth_norm_step)][::-1]
+
                 for featureset in data:
-                    if np.linalg.norm(featureset - centroid) < self.bandwidth: # Euclidean distance
-                        in_bandwidth.append(featureset)
+                    distance = np.linalg.norm(featureset-centroid)
+                    if distance == 0:
+                        distance = 0.000000001
+                    weight_index = int(distance/self.bandwidth)
+                    
+                    if weight_index > self.bandwidth_norm_step - 1:
+                        weight_index = self.bandwidth_norm_step - 1
+                    to_add = (weights[weight_index]**2)*[featureset]
+                    in_bandwidth += to_add
+
+                    #if np.linalg.norm(featureset - centroid) < self.bandwidth: # Euclidean distance
+                    #    in_bandwidth.append(featureset)
                 new_centroid = np.average(in_bandwidth, axis=0)
                 new_centroids.append(tuple(new_centroid))
 
             uniques = sorted(list(set(new_centroids)))
+
+            to_pop = []
+            for i in uniques:
+                for ii in uniques:
+                    if i == ii:
+                        pass
+                    elif np.linalg.norm(np.array(i) - np.array(ii)) <= self.bandwidth:
+                        to_pop.append(ii)
+                        break
+            for i in to_pop:
+                try:
+                    uniques.remove(i)
+                except:
+                    pass
+
+
             prev_centroids = dict(centroids)
 
             centroids = {}
@@ -62,14 +99,32 @@ class Mean_Shift:
         
         self.centroids = centroids
 
+        self.classifications = {}
+
+        for i in range(len(self.centroids)):
+            self.classifications[i] = []
+
+        for featureset in data:
+            distances = [np.linalg.norm(featureset-self.centroids[centroid]) for centroid in self.centroids ]
+            classification = distances.index(min(distances))
+            self.classifications[classification].append(featureset)
+
     def predict(self, data):
-        pass
+        distances = [np.linalg.norm(featureset-self.centroids[centroid]) for centroid in self.centroids ]
+        classification = distances.index(min(distances))
+        return classification
 
 clf = Mean_Shift()
 clf.fit(X)
 
 centroids = clf.centroids
 plt.scatter(X[:, 0], X[:, 1], s=150)
+
+for classification in clf.classifications:
+    color = colors[classification]
+    for featureset in clf.classifications[classification]:
+        plt.scatter(featureset[0], featureset[1], marker='x', color = color, s=150, linewidths=5)
+
 
 for c in centroids:
     plt.scatter(centroids[c][0], centroids[c][1], color='k', marker='*', s=150)
